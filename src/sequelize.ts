20a042ac37c9e537827bb74946c1b8e0d0845305
Robert Damaceno <robertdamacenobob@gmail.com>
2021-08-24 21:40:01 -0300

    eslint

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -4,7 +4,7 @@
 import { Sequelize } from 'sequelize'
 
 const env = process.env.NODE_ENV || `development`
-const config = require(__dirname + `/../configs/database.js`)[env]
+const config = require(`${__dirname}/../configs/database.js`)[env]
 
 const sequelize = new Sequelize(
     config.database,
