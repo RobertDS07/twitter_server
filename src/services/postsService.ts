a3ad67069612913bd27ce08d9e2fc183f35dc881
Robert Damaceno <robertdamacenobob@gmail.com>
2021-09-22 17:03:01 -0300

    feat(postsService.ts): adding method findPosts to handle with find && fix: adding bind in PostsRepository to 'this' inside it be correct 'this'

--- src/services/postsService.ts
+++ src/services/postsService.ts
@@ -1,7 +1,51 @@
+import { IPost, TPostAttributes } from 'models/Posts'
+
 import PostsRepository from 'repositories/PostsRepository'
 
+import Users from 'models/Users'
+
+import { IPaginated } from 'src/interfaces/paginated'
+import { IPropsFind } from 'repositories/_baseClassRepository'
+
+interface IPropsFindPosts
+    extends Partial<Pick<IPropsFind<IPost>, `limit` | `order` | `skip`>> {
+    where?: any
+}
+
+interface IPostsWithCanChange extends TPostAttributes {
+    canChange: boolean
+}
+
 class PostsService {
-    createPost = PostsRepository.create
+    createPost = PostsRepository.create.bind(PostsRepository)
+
+    async findPosts(
+        userId: number,
+        props: IPropsFindPosts,
+    ): Promise<IPaginated<IPostsWithCanChange>> {
+        const { limit, order, skip, where } = props
+
+        const { data: posts, total } = await PostsRepository.find({
+            skip,
+            where,
+            limit,
+            order,
+            include: {
+                model: Users,
+                as: `user`,
+            },
+        })
+
+        const formattedPosts = posts.map(post => ({
+            ...post,
+            canChange: userId === post.userId,
+        }))
+
+        return {
+            data: formattedPosts,
+            total,
+        }
+    }
 }
 
 export default new PostsService()
